a52c2b2649aead0de7ace8b4a72d03d807ca06ba
Denberg28 <111175467+Denberg28@users.noreply.github.com>
2026-09-24 20:10:52 +0800

    Develop TeleRC rover profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
- namespace = "io.github.denberg28.gosim"
+ namespace = "io.github.denberg28.telerc"
  compileSdk = 35
- defaultConfig { applicationId = "io.github.denberg28.gosim"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0"; testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
+ defaultConfig { applicationId = "io.github.denberg28.telerc"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "0.2.0"; testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
  compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
  kotlinOptions { jvmTarget = "17" }
 }
